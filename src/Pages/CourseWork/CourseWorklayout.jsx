e385c0b69be8a66d2ed6770d192896688e4b0a10
Isabel Weinerth <isabelweinerth@gmail.com>
2024-01-29 19:02:20 -0800

    L3 Complete (After Fix up)

--- src/Pages/CourseWork/CourseWorklayout.jsx
+++ src/Pages/CourseWork/CourseWorklayout.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 import Inset from "@/Common/PagesLayout/Inset";
 
