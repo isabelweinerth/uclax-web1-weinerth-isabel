bb5dcb0fad3728446f1473fdd27ba33c81f0585f
Isabel Weinerth <isabelweinerth@gmail.com>
2024-01-29 18:58:16 -0800

    L3 Complete

---
+++ src/Pages/CourseWork/CourseWorklayout.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Outlet } from "react-router-dom";
+
+import Inset from "@/Common/PagesLayout/Inset";
+
+const CourseWorklayout = () => {
+    return (
+        <CourseWorklayoutStyled>
+            <Inset>
+                <h1> Course Work </h1>
+                <nav className="sublinks">
+                    <NavLink to="/course-work">Essays</NavLink>
+                    <NavLink to="/course-work/sun-and-moon">
+                        Sun and Moon
+                    </NavLink>
+                    <NavLink to="/course-work/responsive">Responsive</NavLink>
+                </nav>
+                <Outlet />
+            </Inset>
+        </CourseWorklayoutStyled>
+    );
+};
+
+export default CourseWorklayout;
+
+const CourseWorklayoutStyled = styled.div``;
